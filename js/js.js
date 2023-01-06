c1a13cea341768fb9e25b429e0bd28161547bd6b
Infusca <Infusca192@gmail.com>
2023-01-06 14:31:44 -0500

    Added modal ability to CD art.

--- js/js.js
+++ js/js.js
@@ -2,35 +2,41 @@ $(document).ready(function(){
 
 // regarding old posts "archives"
   $('.archived').hide();
-
   $('#2021').click(function(){
     $('.21').toggle();
   });
-
   $('#2022').click(function(){
     $('.22').toggle();
   });
 
+
+  
   $('.lyrics').hide();
 
-// regarding lyrics
+// get target image and display modal
+$('.cd-img').click(function(e){
+  target = e.target;
+  targetID = target.id;
+  src = target.src;
+  img = getImg(src);
+  var modalImg = $('#modal-img')[0];
+  modalImg.src = '/images/music/' + img;
+  myModal.style.display = 'block';
+})
 
-  // var songList = ['model', 'foolin', 'staying', 'every', 'queen'];
-  // var arr = []
-  //
-  // $('.song').get
-  //
-  // for (i in songList){
-  //   console.log(songList[i]);
-  // };
-  //
-  // $('.song').hide();
-  // $('#model-citizen').show();
+// obtain file name of target image
+function getImg(src){
+  var targetImg;
+  targetImg = src.split();
+  fileName = targetImg[0].split('/')[5];
+  return fileName;
+}
 
 
-
-
-
+// close modal
+$('.close').click(function(){
+  myModal.style.display = 'none';
+})
 
 
 
